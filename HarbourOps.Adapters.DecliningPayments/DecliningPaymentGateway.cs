9052e30c3013d4ed97181093c54334ed0489f2d3
John Watson <johnwatson484@hotmail.com>
2026-06-18 01:07:50 +0100

    Add declining payment gateway

---
+++ HarbourOps.Adapters.DecliningPayments/DecliningPaymentGateway.cs
@@ -0,0 +1,16 @@
+﻿using HarbourOps.Application.Abstractions;
+using HarbourOps.Domain;
+
+namespace HarbourOps.Adapters.DecliningPayments;
+
+public sealed class DecliningPaymentGateway : IPaymentGateway
+{
+    public Task<PaymentResult> ChargeAsync(
+        string customerEmail,
+        Money amount,
+        string description,
+        CancellationToken cancellationToken)
+    {
+        return Task.FromResult(PaymentResult.Failure("Card declined by simulated provider."));
+    }
+}
